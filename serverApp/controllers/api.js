3a76637703068366581812dc62bd427aa98f18aa
wensheng li <lws_home@hotmail.com>
2022-09-23 11:33:55 -0400

    main:

--- serverApp/controllers/api.js
+++ serverApp/controllers/api.js
@@ -7,12 +7,10 @@ exports.api={
     let d=req.body
     console.log(d)
     axios(d).then(function (response) {
-      res.send({
-        status:response.status,
-        data:response.data,
-        headers:response.headers
-      });
-      console.log(response)
+      console.log(res.headers)
+      res.headers=response.headers
+      console.log(res.headers)
+      res.status(response.status).send(response.data);
     }).catch(function (error) {
       console.log(error.message);
       res.status(400).send({status:400,message:error.message})
