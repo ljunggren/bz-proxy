9138df94ef4981cdcbdea0a92233ba9e5c847252
wensheng li <lws_home@hotmail.com>
2022-09-23 11:43:35 -0400

    main:

--- serverApp/controllers/api.js
+++ serverApp/controllers/api.js
@@ -5,14 +5,15 @@ const axios = require('axios');
 exports.api={
   route:async function(req,res){
     let d=req.body
-    console.log("Request:")
-    console.log(d)
     axios(d).then(function (response) {
       res.headers=response.headers
       res.status(response.status).send(response.data);
     }).catch(function (error) {
+      console.log("Request:")
+      console.log(d)
       console.log("Response error:")
       console.log(error.message);
+      console.log(error)
       res.status(400).send({status:400,message:error.message})
     });
 
